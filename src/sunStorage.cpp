ba92ced20ba04aeebac95faf069a8b96df7646ee
Amir-Aref <amirhosseinaref2005@gmail.com>
2024-05-13 12:40:02 +0330

    sunstorage

---
+++ src/sunStorage.cpp
@@ -0,0 +1,15 @@
+#include "sunStorage.hpp"
+
+sunStorage::sunStorage(float x, float y) {
+    if (!sunTexture.loadFromFile("spriters/images/sun.png")) {
+        return;
+    }
+    sprite.setTexture(sunTexture);
+    sprite.setScale(0.7,0.7);
+    sprite.setPosition(x, y);
+}
+
+void sunStorage::render(RenderWindow &window) {
+    window.draw(sprite);
+}
+
